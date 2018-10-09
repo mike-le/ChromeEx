85a7b9317112b805c3b22ef61dfa809526ff7a49
mike-le <michael.le@richmond.edu>
2018-10-08 22:06:22 -0400

    Add functionality for language translation

---
+++ content_script.js
@@ -0,0 +1,41 @@
+walk(document.body);
+
+function walk(node) 
+{
+    //Inspired by https://github.com/panicsteve/cloud-to-butt
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bword\b/g, "kotoba");
+	v = v.replace(/\bWord\b/g, "言葉");
+	
+	textNode.nodeValue = v;
+}
