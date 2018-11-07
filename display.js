715128923ece027444f822c05c9e9b8fa57bb368
mike-le <michael.le@richmond.edu>
2018-11-06 19:09:35 -0500

    Fix toggle display on empty dataset bug

--- display.js
+++ display.js
@@ -30,13 +30,17 @@ getValue(function (items) {
 function showWords(){
     event.preventDefault();
     var x = document.getElementById("dictionary");
-    if (x.style.opacity != 1) {
-        x.style.opacity = 1;
-        x.style.minHeight = "200px";
-    } else {
-        x.style.opacity = 0;
-        x.style.minHeight = "0px";
-    }
+    getValue(function (items) {
+        if(Object.keys(items).length > 0){
+            if (x.style.opacity != 1) {
+                x.style.opacity = 1;
+                x.style.minHeight = "200px";
+            } else {
+                x.style.opacity = 0;
+                x.style.minHeight = "0px";
+            }
+        }
+    }); 
 }
 
 function getValue(callback) {
